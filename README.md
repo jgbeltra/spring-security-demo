cd06e49bc2bf9231b0ad3b1efcb2aef6acf10c77
BELTJ051 <John.X.Beltran.-ND@disney.com>
2019-08-27 17:03:13 -0500

    Este commit permite implementar spring security usando un usario por defecto

---
+++ README.md
@@ -0,0 +1,9 @@
+# spring-secutiry-demo
+
+1) Ejecute el servicio con el siguiente comando:
+mvn spring-boot:run
+
+2) El endpoint disponible esta en la raiz del proyecto
+http://localhost:8080/
+
+3) Referencia -> https://spring.io/guides/gs/securing-web/
--- src/main/java/co/com/company/config/MvcConfig.java
+++ src/main/java/co/com/company/config/MvcConfig.java
@@ -10,7 +10,12 @@ public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
 
-        registry.addViewController("/").setViewName("index");
+        //registry.addViewController("/").setViewName("index");
+
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
 
     }
 
