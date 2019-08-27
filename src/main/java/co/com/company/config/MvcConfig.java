79d7c37b63a789b41e08db189c2443931eb6b5c7
johan beltran <jgbeltra@gmail.com>
2019-08-27 11:23:01 -0500

    Se agrega configuracion basica con thymeleaf

---
+++ src/main/java/co/com/company/config/MvcConfig.java
@@ -0,0 +1,17 @@
+package co.com.company.config;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Controller
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+
+        registry.addViewController("/").setViewName("index");
+
+    }
+
+}
